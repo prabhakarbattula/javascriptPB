c9fd073a67c469509a4d33053c39150ac9580906
PB <pb@PBs-MacBook-Pro.local>
2017-11-24 22:09:48 +0530

    Modified variables file as the action on colour picket was not working.  Fixed it.

--- 03-variables-js/variables.js
+++ 03-variables-js/variables.js
@@ -2,6 +2,7 @@
 
   const spacing = document.querySelector('input#spacing');
   const blur = document.querySelector('input#blur');
+  const base = document.querySelector('#base');
 
     function handleUpdateSpacing() { 
       const suffix = this.dataset.sizing || '';  
@@ -14,7 +15,14 @@
       document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix);
       document.getElementById('blurOutput').innerHTML = this.value; 
     }
+
+    function handleUpdateColour() { 
+      const suffix = this.dataset.sizing || '';  
+      document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix);
+      //document.getElementById('blurOutput').innerHTML = this.value; 
+    }
     
     //inputs.forEach(input => input.addEventListener('change' || 'mousemove', handleUpdate));
     spacing.addEventListener('change' || 'mousemove', handleUpdateSpacing);
        blur.addEventListener('change' || 'mousemove', handleUpdateBlur);
+       base.addEventListener('change' || 'mousemove', handleUpdateColour);
